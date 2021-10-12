96ed89fbf842557bfd470c97766d47da7d7a39d1
johnxandr <80538549+johnxandr@users.noreply.github.com>
2021-10-13 02:16:36 +0300

    Add files via upload

---
+++ john-yemi.py
@@ -0,0 +1,6 @@
+name = ("John Adeoye")
+mail = ("john.adeoye@outlook.com")
+slack_username = ("@john-yemi")
+biostack = ("Preffered BioStack is Genomics")
+
+print (name, mail, slack_username, biostack,sep=", " )
