5e6fe1522080758a8c6817367b5f313272e49cc1
Vliholl <92336117+Vliholl@users.noreply.github.com>
2021-10-14 21:52:43 +0200

    Create script perl

---
+++ joaquin-espi.pl
@@ -0,0 +1,9 @@
+#!/usr/local/bin/perl
+ 
+ $name = 'Joaquin Espi';
+ $email = 'j.espi.soler@gmail.com';
+ $language = 'Perl';
+ $biostack = 'Drug discovery';
+ $slack = '@vliholl';
+ 
+ print "$name, $email, $language, $biostack, $slack";
