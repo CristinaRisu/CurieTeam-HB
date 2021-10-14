22dd65014cd91c7a556b9d7ac55f5873c93ba677
Risu <92274646+CristinaRisu@users.noreply.github.com>
2021-10-14 15:46:06 +0200

    Add files via upload

---
+++ CrisRisu.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "Cristina Riobello", email = "cristinarisu15@gmail.com", stack username = "@CrisRisu", biostack = "Transcriptomics");
+    
+    cout << name<<"  "<< email<<"  "<< stack username <<"  "<< biostack<<"  "<<endl;
+    return 0;
+}
