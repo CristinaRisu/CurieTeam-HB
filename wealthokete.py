01de188d1063f78404f19c7514bef195acb08d0c
SirW21 <88290322+SirW21@users.noreply.github.com>
2021-10-15 13:44:06 +0100

    Add files via upload

---
+++ wealthokete.py
@@ -0,0 +1,8 @@
+#personal details
+
+Name = 'Wealth Okete'
+Email = 'wealth.okete@gmail.com'
+Slack_Username = '@wealthokete'
+Biostack = 'Genomics'
+
+print (Name, Email, Slack_Username, Biostack, sep = '/n')
